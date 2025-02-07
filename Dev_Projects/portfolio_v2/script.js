de03c3ad44f29b11c8d3554ca4f0ae6ee6a32f73
Rykala Tomasz <100706530+rykala79@users.noreply.github.com>
2025-02-07 10:35:28 +0100

    06.02.25

---
+++ Dev_Projects/portfolio_v2/script.js
@@ -0,0 +1,20 @@
+const resumeLists = document.querySelectorAll('.resume-list');
+const resumeBoxs = document.querySelectorAll('.resume-box');
+
+//navbar actions and all section actions along with cube rotation when navbar is clicked
+
+//resume section when clicking tab-list
+
+resumeLists.forEach((list, idx)=>{
+    list.addEventListener('click', ()=> {
+        document.querySelector('.resume-list.active').classList.remove('active');
+        list.classList.add('active');
+
+        document.querySelector('.resume-box.active').classList.remove('active');
+        resumeBoxs[idx].classList.add('active');
+    });
+});
+
+// portfolio section when clicking tab-list
+
+//visibility for contact section when reloading (cube reloading animation)
